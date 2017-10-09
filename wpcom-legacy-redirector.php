c5fa739df802d1cefe9045cbbea5e0e1b1010d6d
Derrick Tennant <emrikol@users.noreply.github.com>
2017-10-09 17:07:51 -0400

    Adds basic core editing to the post type

--- wpcom-legacy-redirector.php
+++ wpcom-legacy-redirector.php
@@ -33,12 +33,121 @@ class WPCOM_Legacy_Redirector {
 	static function start() {
 		add_action( 'init', array( __CLASS__, 'init' ) );
 		add_filter( 'template_redirect', array( __CLASS__, 'maybe_do_redirect' ), 0 ); // hook in early, before the canonical redirect
+		add_action( 'add_meta_boxes', array( __CLASS__, 'add_meta_boxes' ) );
+		add_action( 'save_post_' . self::POST_TYPE, array( __CLASS__, 'save_post' ) );
+		add_filter( 'post_updated_messages', array( __CLASS__, 'updated_messages' ) );
+		add_filter( 'manage_' . self::POST_TYPE . '_posts_columns', array( __CLASS__, 'custom_columns' ), 10, 1 );
 	}
 	
+	static function add_meta_boxes() {
+		add_meta_box(
+			'redirect-details',
+			esc_html__( 'Redirect Details', 'vip-legacy-redirect' ),
+			array( __CLASS__, 'add_meta_box_callback' ),
+			self::POST_TYPE,
+			'normal',
+			'high'
+		);
+	}
+
+	static function add_meta_box_callback( $post ) {
+		wp_nonce_field( 'redirect_details_data', 'redirect_details_nonce' );
+
+		echo '<p><label class="screen-reader-text" for="post-parent-id">Redirect Post ID</label>';
+		echo '<input class="post-parent-id" size="4" name="post-parent-id" id="post-parent-id" type="number" value="' . absint( $post->post_parent ) . '" /></p>';
+		echo '<p class="howto" id="post-parent-id-desc">If a non-zero Post ID is entered, it will redirect to that post.</p>';
+		echo '<p><label class="screen-reader-text" for="redirect-url">Redirect URL</label>';
+		echo '<input class="regular-text" name="redirect-url" id="redirect-url" type="text" value="' . esc_url( $post->post_excerpt ) . '" /></p>';
+		echo '<p class="howto" id="redirect-url-desc">If no Post ID is entered, it will redirect to this URL.</p>';
+		echo '<input class="regular-text" disabled=disabled type="text" id="redirect-hash" value="' . esc_attr( $post->post_name ) . '" /></p>';
+		echo '<p class="howto" id="redirect-hash">Redirect Hash (Read Only)</p>';
+
+	}
+
+	static function save_post( $post_id ) {
+		remove_action( 'save_post_' . self::POST_TYPE, array( __CLASS__, 'save_post' ) );
+		if ( ! isset( $_POST['redirect_details_nonce'] ) ) {
+			return false;
+		}
+
+		if ( ! wp_verify_nonce( $_POST['redirect_details_nonce'], 'redirect_details_data' ) ) {
+			return false;
+		}
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return false;
+		}
+		$from_url = sanitize_text_field( $_POST['post_title'] );
+		$redirect_to = (bool) $_POST['post-parent-id'] ? (int) $_POST['post-parent-id'] : esc_url_raw( $_POST['redirect-url'] );
+		self::insert_legacy_redirect( $from_url, $redirect_to, $post_id );
+	}
+
 	static function init() {
-		register_post_type( self::POST_TYPE, array(
+		$args = array(
 			'public' => false,
-		) );
+		);
+
+		if ( current_user_can( apply_filters( 'wpcom_legeacy_redirector_show_ui_capability', 'manage_options' ) ) ) {
+			$labels = array(
+				'name' => esc_html_x( 'Legacy Redirects', 'Legacy Redirects General Name', 'wpcom-legacy-redirector' ),
+				'singular_name' => esc_html_x( 'Legacy Redirects', 'Legacy Redirects Singular Name', 'wpcom-legacy-redirector' ),
+				'menu_name' => esc_html__( 'Legacy Redirects', 'wpcom-legacy-redirector' ),
+				'name_admin_bar' => esc_html__( 'Legacy Redirects', 'wpcom-legacy-redirector' ),
+				'archives' => esc_html__( 'Redirect Archives', 'wpcom-legacy-redirector' ),
+				'attributes' => esc_html__( 'Redirect Attributes', 'wpcom-legacy-redirector' ),
+				'parent_item_colon' => esc_html__( 'Parent Redirect:', 'wpcom-legacy-redirector' ),
+				'all_items' => esc_html__( 'All Redirects', 'wpcom-legacy-redirector' ),
+				'add_new_item' => esc_html__( 'Add New Redirect', 'wpcom-legacy-redirector' ),
+				'add_new' => esc_html__( 'Add New', 'wpcom-legacy-redirector' ),
+				'new_item' => esc_html__( 'New Redirect', 'wpcom-legacy-redirector' ),
+				'edit_item' => esc_html__( 'Edit Redirect', 'wpcom-legacy-redirector' ),
+				'update_item' => esc_html__( 'Update Redirect', 'wpcom-legacy-redirector' ),
+				'view_item' => esc_html__( 'View Redirect', 'wpcom-legacy-redirector' ),
+				'view_items' => esc_html__( 'View Redirects', 'wpcom-legacy-redirector' ),
+				'search_items' => esc_html__( 'Search Redirect', 'wpcom-legacy-redirector' ),
+				'not_found' => esc_html__( 'Not found', 'wpcom-legacy-redirector' ),
+				'not_found_in_trash' => esc_html__( 'Not found in Trash', 'wpcom-legacy-redirector' ),
+				'featured_image' => esc_html__( 'Featured Image', 'wpcom-legacy-redirector' ),
+				'set_featured_image' => esc_html__( 'Set featured image', 'wpcom-legacy-redirector' ),
+				'remove_featured_image' => esc_html__( 'Remove featured image', 'wpcom-legacy-redirector' ),
+				'use_featured_image' => esc_html__( 'Use as featured image', 'wpcom-legacy-redirector' ),
+				'insert_into_item' => esc_html__( 'Insert into redirect', 'wpcom-legacy-redirector' ),
+				'uploaded_to_this_item' => esc_html__( 'Uploaded to this redirect', 'wpcom-legacy-redirector' ),
+				'items_list' => esc_html__( 'Redirects list', 'wpcom-legacy-redirector' ),
+				'items_list_navigation' => esc_html__( 'Redirects list navigation', 'wpcom-legacy-redirector' ),
+				'filter_items_list' => esc_html__( 'Filter redirect list', 'wpcom-legacy-redirector' ),
+			);
+			$args = array_merge( $args, array(
+				'show_ui' => true,
+				'show_in_admin_bar' => false,
+				'menu_icon' => 'dashicons-randomize',
+				'capability_type' => array( 'legacy_redirect', 'legacy_redirects' ),
+				'label' => esc_html__( 'Legacy Redirects', 'wpcom-legacy-redirector' ),
+				'labels' => $labels,
+				'supports' => array(
+					'title',
+				),
+				'rewrite' => false,
+				'query_var' => false,
+				'delete_with_user' => false,
+				'show_in_rest' => false,
+			) );
+		}
+		register_post_type( self::POST_TYPE, $args );
+	}
+
+	static function custom_columns( $columns ) {
+		// Rename the title.
+		if ( 'Title' === $columns['title'] ) {
+			$columns['title'] = 'Legacy Path';
+		}
+
+		// We don't need to show Likes.
+		if ( isset( $columns['likes'] ) ) {
+			unset( $columns['likes'] );
+		}
+
+		return $columns;
 	}
 
 	static function maybe_do_redirect() {
@@ -71,7 +180,7 @@ class WPCOM_Legacy_Redirector {
  	 * @param int|string $redirect_to The post ID or URL to redirect to.
  	 * @return bool|WP_Error Error if invalid redirect URL specified or if the URI already has a rule; false if not is_admin, true otherwise.
  	 */
-	static function insert_legacy_redirect( $from_url, $redirect_to ) {
+	static function insert_legacy_redirect( $from_url, $redirect_to, $post_id = 0 ) {
 
 		if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) && ! is_admin() && ! apply_filters( 'wpcom_legacy_redirector_allow_insert', false ) ) {
 			// never run on the front end
@@ -90,7 +199,9 @@ class WPCOM_Legacy_Redirector {
 		}
 
 		$args = array(
+			'ID' => $post_id,
 			'post_name' => $from_url_hash,
+			'post_status' => 'draft',
 			'post_title' => $from_url,
 			'post_type' => self::POST_TYPE,
 		);
@@ -225,6 +336,26 @@ class WPCOM_Legacy_Redirector {
 
 		return $normalised_url;
 
+	}
+
+	static function updated_messages( $messages ) {
+		$post = get_post();
+
+		$messages[ self::POST_TYPE ] = array(
+			1  => 'Legacy Redirect updated.',
+			4  => 'Legacy Redirect updated.',
+			5  => isset( $_GET['revision'] ) ? sprintf( 'Legacy Redirect restored to revision from %s',wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+			6  => 'Legacy Redirect published.',
+			7  => 'Legacy Redirect saved.',
+			8  => 'Legacy Redirect submitted.',
+			9  => sprintf(
+				'Legacy Redirect scheduled for: <strong>%1$s</strong>.',
+				date_i18n( 'M j, Y @ G:i', strtotime( $post->post_date ) )
+			),
+			10 => 'Legacy Redirect draft updated.'
+		);
+
+		return $messages;
 	}
 }
 
